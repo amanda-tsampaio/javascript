66e0ba8aa7f4767cf35039751be810cb12676e2f
amanda-tsampaio <amandasampaio236@gmail.com>
2025-03-25 10:17:55 -0300

    NEW

---
+++ Exercicios/ex008/ex008.js
@@ -0,0 +1,12 @@
+var vel = 60.5;
+if (vel > 70) {
+  console.log(`A velocidade é ${vel}`);
+} else {
+  console.log(`Dirija sempre usando cinto`);
+}
+
+var pais = "EUA";
+console.log(`Vivendo em ${pais}`);
+if (pais != "BRASIL") {
+  console.log(`Você é estrangeiro!`);
+}
